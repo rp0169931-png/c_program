1436a8567cf6d7c375e932e6952d463c349e14ae
Manas Singh <rp0169931@gmail.com>
2026-07-13 14:47:59 +0530

    mark.c

---
+++ mark.c
@@ -0,0 +1,27 @@
+# include<stdio.h>
+int main(void){
+    float total=0 ,mark, persentage;
+printf("Enter the mark of subject:maths,science,english,hindi,computer: ");
+for(int i=0;i<5;i++){
+    scanf("%f",&mark);
+    total+=mark;
+}
+persentage=(total/500)*100;
+if(persentage>=90){
+    printf("Grade A");
+}
+else if(persentage>=75){
+    printf("Grade B");
+}
+else if(persentage>=65){
+    printf("Grade C");
+}
+else if(persentage>=40){
+    printf("Grade D");
+}
+else{
+    printf("Fail");
+}
+printf("\nPersentage: %.2f",persentage);
+return 0;
+}
